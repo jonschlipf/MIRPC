4bcf89a92e79b69b674e9eb0c9d66edd36c0d32f
Jon Schlipf <schlipf@b-tu.de>
2022-05-10 17:40:57 +0200

    spcm gui

--- src/qtgui_sample.py
+++ src/qtgui_sample.py
@@ -52,10 +52,46 @@ class QtGuiSample(QVBoxLayout):
         sample_pos_layout.addWidget(self.sample_z_label)
         sample_pos_layout.addWidget(self.sample_z_box)
         sample_pos_layout.addWidget(self.sample_pos_button)
+        
+        self.sample_xmin_label=QLabel('x min')
+        self.sample_xmin_box=QLineEdit()
+        self.sample_xmin_box.setText("0")
+        self.sample_xmax_label=QLabel('x max')
+        self.sample_xmax_box=QLineEdit()
+        self.sample_xmax_box.setText("0")
+        self.sample_ymin_label=QLabel('y min')
+        self.sample_ymin_box=QLineEdit()
+        self.sample_ymin_box.setText("0")
+        self.sample_ymax_label=QLabel('y max')
+        self.sample_ymax_box=QLineEdit()
+        self.sample_ymax_box.setText("0")
+        self.sample_spcm_button=QPushButton('Map')
+        self.sample_spcm_button.clicked.connect(self.sample_spcm_button_clicked)
+        self.sample_opt_button=QPushButton('Maximize')
+        self.sample_opt_button.clicked.connect(self.sample_opt_button_clicked)
+        sample_min_layout=QHBoxLayout()
+        sample_min_layout.addWidget(self.sample_xmin_label)
+        sample_min_layout.addWidget(self.sample_xmin_box)
+        sample_min_layout.addWidget(self.sample_ymin_label)
+        sample_min_layout.addWidget(self.sample_ymin_box)
+        sample_min_layout.addWidget(self.sample_spcm_button)
+        sample_max_layout=QHBoxLayout()
+        sample_max_layout.addWidget(self.sample_xmax_label)
+        sample_max_layout.addWidget(self.sample_xmax_box)
+        sample_max_layout.addWidget(self.sample_ymax_label)
+        sample_max_layout.addWidget(self.sample_ymax_box)
+        sample_max_layout.addWidget(self.sample_opt_button)
+
 
         self.addLayout(sample_bias_layout)
         self.addLayout(sample_irange_layout)
         self.addLayout(sample_pos_layout)
+        self.addLayout(sample_min_layout)
+        self.addLayout(sample_max_layout)
+    def sample_spcm_button_clicked(self):
+        pass
+    def sample_opt_button_clicked(self):
+        pass
     def sample_irange_button_clicked(self):
         try:
             self.lockin.set_input_current_range(float(self.sample_irange_box.text()))
